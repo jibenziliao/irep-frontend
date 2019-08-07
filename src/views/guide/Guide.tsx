80a20a09e121c7ba7c75add22e2b05e86a3823df
Jiajia Qian <1023313303@qq.com>
2019-08-07 23:55:24 +0800

    实验引导视频

--- src/views/guide/Guide.tsx
+++ src/views/guide/Guide.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import styles from "./Guide.module.less"
 
 const Guide = () => {
   return (
-    <div className="GlobalContainer">
-      <h1>实验指导</h1>
+    <div className={styles.Container}>
+      <div className={styles.topContainer}>
+        <div className={styles.topTitle}>实验指导</div>
+        <div className={styles.subTitle}>Experimental Instruction</div>
+      </div>
+      <div className={styles.videoContainer}>
+          放引导视频
+      </div>
     </div>
   )
 }
