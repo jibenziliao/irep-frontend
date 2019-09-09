d411c1e13da53185ba61085c93ee184455236dca
jibenziliao <1501718947@qq.com>
2019-09-10 04:10:41 +0800

    feat: request.ts增加调试代码

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -49,6 +49,7 @@ export const requestFn = (dispatch: Dispatch<Actions>, params: Params): AxiosPro
         return response
       },
       error => {
+        console.log(error)
         if (error.response && error.response.status === 401 && error.response.config.url.indexOf('/login') === -1) {
           tokenExpired()
           return reject(error)
